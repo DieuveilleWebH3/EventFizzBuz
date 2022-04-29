562d2a039d2db472ce95bb589b13fa03ba8b2d36
DieuveilleWebH3 <d.boussaellenga@h3hitema.fr>
2022-04-29 13:01:59 +0200

    OK: using alternative way to class, bug not fixed yet, ...

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -2,14 +2,19 @@
 // requesting the event module
 const FizzBuzz = require('events');
 
-class Logger extends FizzBuzz()
-{
-    // function to take the arg and return the approriate answer
-    log(the_number)
-    {
-        if(typeof(the_number) !== 'number') return "You have not entered a number";
-    }
+// class Logger extends FizzBuzz()
+// {
+//     // function to take the arg and return the approriate answer
+//     log(the_number)
+//     {
+//         if(typeof(the_number) !== 'number') return "You have not entered a number";
+//     }
 
-}
+// }
 
 // module.exports = Logger;
+
+
+
+const my_fizz_buzz = new FizzBuzz();
+
