730dd2ddc8b44976b111f7a9ba7b9e0db9fa3db2
DieuveilleWebH3 <d.boussaellenga@h3hitema.fr>
2022-04-29 12:47:17 +0200

    KO: error, Cannot read properties of undefined, ..

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -1,3 +1,9 @@
 
 // requesting the event module
-const fizzBuzz = require('events');
+const FizzBuzz = require('events');
+
+class Logger extends FizzBuzz()
+{
+    // function to take the arg and return the approriate answer
+    
+}
